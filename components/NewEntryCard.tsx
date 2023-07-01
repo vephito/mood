3642206f89a2082a54bf5024a8129b40c0637bc8
V <vephitodasai5@gmail.com>
2023-07-01 21:38:31 +0530

    init

--- components/NewEntryCard.tsx
+++ components/NewEntryCard.tsx
@@ -2,7 +2,7 @@ const NewEntryCard = () => {
   return (
     <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow">
       <div className="px-4 py-5 sm:p-6">
-        <span className="text-3xl">New Entry Card</span>
+        <span className="text-3xl">NewEntryCard</span>
       </div>
     </div>
   );
